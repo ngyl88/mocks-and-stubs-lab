f2fbf8805049f6ee950fa15165dc6fe4ab948187
ngyl88 <ng_yl88@hotmail.com>
2018-07-09 18:08:09 +0800

    added queueService tests

--- test/queueService.test.js
+++ test/queueService.test.js
@@ -1,3 +1,27 @@
-it("", () => {
-// 
+const mockRandomInt = jest.fn();
+jest.doMock("mathjs", () => {
+  return {
+    randomInt: mockRandomInt
+  };
+});
+const generateQueue = require("../src/queueService");
+
+beforeEach(() => {
+  mockRandomInt.mockReset();
+  mockRandomInt
+    .mockImplementationOnce((num1, num2) => 3)
+    .mockImplementationOnce((num1, num2) => 2)
+    .mockImplementationOnce((num1, num2) => -10)
+    .mockImplementationOnce((num1, num2) => 1);
+});
+
+it("should call randomInt when generateQueue is called", () => {
+  generateQueue();
+  expect(mockRandomInt).toBeCalledTimes(4);
+});
+
+it("should return an array when generateQueue is called", () => {
+  const result = generateQueue();
+  expect(result).toBeInstanceOf(Array);
+  expect(result).toEqual([2, -10, 1]);
 });
